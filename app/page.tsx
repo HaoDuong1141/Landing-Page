9cb8926acc41eae139abc8f80f023a57179b53db
Hao Duong <haoduong1141@gmail.com>
2023-12-23 14:27:06 -0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,76 @@
-import Image from 'next/image'
+import Image from "next/image";
+import NavBar from "@/components/NavBar";
+import { Lato } from "next/font/google";
+
+const lato = Lato({
+    subsets: ["latin"],
+    weight: "400",
+});
+
+const backgroundStyle = {
+    "background-image": `linear-gradient(
+                            to bottom,
+                            #1c1c1c,
+                            transparent 100%
+                            ), 
+                            url("/bg.png")`,
+    height: "100vh",
+    width: "100%",
+};
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    return (
+        <main className={lato.className}>
+            <NavBar />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <div className="h-[400vh] text-white">
+                <div className="h-[100vh]">
+                    <div
+                        className="absolute h-[100vh] bg-cover bg-gradient-to-t from-white to-black"
+                        style={backgroundStyle}
+                    >
+                        <div className="flex flex-col justify-center items-center text-white gap-16 relative">
+                            <div className="text-8xl font-extrabold pt-[25vh]">
+                                Your New{" "}
+                                <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#147390] to-[#A6FBB2]">
+                                    Copilot
+                                </span>
+                            </div>
+                            <div className="w-[70%] text-6xl text-center font-bold leading-snug">
+                                Constantly Accessible, Completely Anonymous,
+                                Contextually Aware
+                            </div>
+                            <button className="py-5 px-11 rounded bg-gradient-to-r from-[#106884] to-[#88CDE1] text-xl">
+                                Smart Career Benchmark
+                            </button>
+                        </div>
+                    </div>
+                </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                <div className="h-[100vh] flex flex-col justify-center items-center gap-10 bg-gradient-to-br from-[#01171D] to-[#1B404D]">
+                    <div className="text-6xl font-semibold">
+                        Considering a Job Change? Unsure If Better Awaits?
+                    </div>
+                    <div className="text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#147390] to-[#A6FBB2]">
+                        Smart Career Benchmark
+                    </div>
+                    <div className="text-2xl">
+                        Compare, Analyza, and Intelligent Guidance for Success
+                    </div>
+                    <button className="py-4 px-12 rounded bg-gradient-to-r from-[#106884] to-[#88CDE1] text-xl">
+                        Smart Career Benchmark
+                    </button>
+                    <p className="w-[830px] text-2xl text-center">
+                        An insightful and private tool that quickly assesses
+                        your career standing through 16 targeted questions,
+                        offering comparative insights with peers and
+                        personalized strategies for growth, ideal for those at
+                        career crossroads.
+                    </p>
+                </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+                <div className="h-[200vh] bg-slate-900"></div>
+            </div>
+        </main>
+    );
 }
---
+++ components/NavBar.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Link from "next/link";
+import { Orbitron, Lato } from "next/font/google";
+
+const orbitron = Orbitron({ subsets: ["latin"], weight: "400" });
+const lato = Lato({
+    subsets: ["latin"],
+    weight: "400",
+});
+
+const NavBar = () => {
+    return (
+        <div
+            className={`flex flex-row justify-between items-center bg-black h-[10vh] text-white text-2xl p-12 ${lato.className}`}
+        >
+            <div className="flex gap-16 items-center">
+                <Link href="/" className={`text-3xl ${orbitron.className}`}>
+                    CopilotCareer
+                </Link>
+                <Link href="/">Home</Link>
+                <Link href="/">Smart Benchmark</Link>
+                <Link href="/">Augmented Mentorship</Link>
+                <Link href="/">Enerprise Solution</Link>
+                <Link href="/">Contact</Link>
+            </div>
+            <button className="py-1 px-8 rounded bg-gradient-to-r from-[#106884] to-[#88CDE1]">
+                Log in
+            </button>
+        </div>
+    );
+};
+
+export default NavBar;
